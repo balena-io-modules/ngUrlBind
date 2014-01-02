08102e61f72f7fcf82ded047d28221a652091997
AlexandrosM <alexandros@rulemotion.com>
2014-01-02 12:29:10 +0000

    comment correction

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 //the build instructions are in gulpfile.coffee
 //this file is a simple bridge since gulp doesn't support
-//coffeescript files natively.
+//coffeescript files without an ungodly command line arg
 
 require("coffee-script")
 require("./gulpfile.coffee")
